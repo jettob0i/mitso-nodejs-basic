8780eba028c7df85d695370ff7d2d90970a1ffd4
jeet0 <kage.zama.04@gmail.com>
2025-05-20 20:53:42 +0300

    feat: write.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -2,7 +2,7 @@ import { createReadStream } from 'fs';
 import path from 'path';
 
 const read = async () => {
-    const filePath = path.resolve('mitso-nodejs-basic/src/streams/files/fileToRead.txt');
+    const filePath = path.resolve('src/streams/files/fileToRead.txt');
     const stream = createReadStream(filePath, 'utf-8');
     stream.pipe(process.stdout);
 };
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+
 const write = async () => {
-    // Write your code here 
+    const filePath = join('src/streams/files/fileToWrite.txt');
+    const writableStream = createWriteStream(filePath);
+
+    process.stdin.pipe(writableStream);
 };
 
 await write();
