f54b3c63c4f6aa9083f3a367b70b41c941d0078a
jeet0 <kage.zama.04@gmail.com>
2025-05-20 20:38:38 +0300

    feat: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.resolve('mitso-nodejs-basic/src/hash/files/fileToCalculateHashFor.txt');
+    const fileContent = await readFile(filePath);
+    const hash = createHash('sha256').update(fileContent).digest('hex');
+    console.log(hash);
 };
 
 await calculateHash();
