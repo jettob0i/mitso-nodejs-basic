f6f641288969ef47f1deb2e6b8c54de9ba7fef78
jeet0 <kage.zama.04@gmail.com>
2025-05-20 19:45:04 +0300

    feat: copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,37 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import { dirname } from 'path'; //для dirname, плохо ищет путь файла
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+    const sourceFolder = join(__dirname, 'files');
+    const destinationFolder = join(__dirname, 'files_copy');
+
+    try {
+
+        await fs.access(sourceFolder);
+
+        try {
+            await fs.access(destinationFolder);
+            throw new Error('FS operation failed');
+        } catch (err) {
+            if (err.code !== 'ENOENT') throw err;
+        }
+
+        await fs.mkdir(destinationFolder);
+
+        const files = await fs.readdir(sourceFolder);
+        for (const file of files) {
+            const sourceFile = join(sourceFolder, file);
+            const destinationFile = join(destinationFolder, file);
+
+            await fs.copyFile(sourceFile, destinationFile);
+        }
+    } catch (err) {
+        throw err;
+    }
 };
 
 await copy();
