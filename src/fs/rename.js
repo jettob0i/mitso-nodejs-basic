c25a3fdc03fde791cb26644db4c36ce871a55366
jeet0 <kage.zama.04@gmail.com>
2025-05-20 20:02:26 +0300

    feat: rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+    const oldFilePath = join(__dirname, 'files', 'properFilename1.md');
+    const newFilePath = join(__dirname, 'files', 'properFilename.md');
+
+    try {
+        await fs.access(oldFilePath);
+
+        try {
+            await fs.access(newFilePath);
+            throw new Error('FS operation failed');
+        } catch (err) {
+            if (err.code !== 'ENOENT') throw err;
+        }
+
+        await fs.rename(oldFilePath, newFilePath);
+        console.log("файл переименован\n");
+    } catch (err) {
+        console.log("FS operation failed");
+    }
 };
 
 await rename();
