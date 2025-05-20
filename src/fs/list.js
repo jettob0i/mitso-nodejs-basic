762350e1f8e8e5bf53a39ce4154fd6964f2dd196
jeet0 <kage.zama.04@gmail.com>
2025-05-20 20:09:17 +0300

    feat: list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const directoryPath = join(__dirname, 'files');
+
+  try {
+    await fs.access(directoryPath);
+
+    const files = await fs.readdir(directoryPath);
+
+    console.log(files);
+  } catch {
+    console.log('FS operation failed');
+  }
 };
 
 await list();
